dd7c687f023486a6a2fba1e25da8bebf82041923
泽元 吴 <potter@hinetcloud.com>
2018-08-28 13:36:10 +0800

    demo
    demo

---
+++ DemoLib.h
@@ -0,0 +1,14 @@
+//
+//  DemoLib.h
+//  DemoLib
+//
+//  Created by wenxing on 2018/8/24.
+//  Copyright © 2018年 wenxing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface DemoLib : NSObject
+-(void)cmd:(UINavigationController *)controller    cmd:(int)cmdCode;
+@end
